e4182295d1d9063d464db21473944230af181705
xfaramir <xfaramir@gmail.com>
2020-11-19 14:17:13 -0500

    gitignore added

---
+++ index.js
@@ -0,0 +1,146 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
+
+// Load client secrets from a local file.
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err)
+        return console.error(
+          "Error while trying to retrieve access token",
+          err
+        );
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+  const sheets = google.sheets({ version: "v4", auth });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1B3834S8a1WMwZ4uHGiui-01kR0KfzwOAG_nhFYbfaho",
+      range: "A2:E",
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        console.log("Element, English:");
+        // Print columns A and E, which correspond to indices 0 and 4.
+        productHero(rows);
+      } else {
+        console.log("No data found.");
+      }
+    }
+  );
+}
+
+function productHero(rows) {
+  console.log(rows[0]);
+  HeroProduct.productHero.eyebrow = rows[0][1];
+  HeroProduct.productHero.headline = rows[1][1];
+  HeroProduct.productHero.textOne = rows[2][1];
+  HeroProduct.productHero.textTwo = rows[3][1];
+  HeroProduct.productHero.cta[0].text = rows[4][1];
+  HeroProduct.productHero.cta[1].text = rows[5][1];
+
+  console.log(HeroProduct);
+}
+
+let HeroProduct = {
+  productHero: {
+    video: {
+      cover: {
+        small: "../../images/product-hero/glycerine-poster-s.jpg",
+        large: "../../images/product-hero/glycerine-poster.jpg",
+        alt: "Placeholder",
+      },
+      src: {
+        large:
+          "../../images/product-hero/S21_Glycerin-19_Launch_XL_Hero_v4.mp4",
+        small: "../../images/product-hero/S21_Glycerin-19_Launch_S_Hero_v4.mp4",
+      },
+    },
+    cta: [
+      {
+        link: "#",
+        text: "",
+        type: "primary",
+      },
+      {
+        link: "#",
+        text: "",
+        type: "primary",
+      },
+    ],
+    headline: "",
+    eyebrow: "",
+    textOne: "",
+    textTwo: null,
+    theme: "theme--dark--pink",
+  },
+};
